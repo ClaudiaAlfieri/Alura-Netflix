6cad22e28e2a481a8af828f0ebb62a06d8662a9d
Claudia Alfieri <claudia.jorge86@gmail.com>
2026-03-30 11:16:04 +0100

    Update data.js

--- catalogo/js/data.js
+++ catalogo/js/data.js
@@ -1,41 +1,167 @@
-export const categories = [
+export const catalogosPorPerfil = {
+    João: [
     {
-        title: "Épicos",
+        title: "Mais assistidos",
         items: [
             {
-                img: "https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg", top10: true, badge: "Clássico", badgeColor: "red", progress: 0,
-                youtube: "https://www.youtube.com/watch?v=bLvqoHBptjg"
+                img: "https://static.wikia.nocookie.net/world-of-media/images/b/be/Nausicaa.jpg/revision/latest/thumbnail/width/360/height/360?cb=20210415190740", top10: true, badge: "Clássico", badgeColor: "green", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=zaskbIZtfxQ"
             },
             {
-                img: "https://aventurasnahistoria.com.br/wp-content/uploads/entretenimento/gladiador_2_VvnGVes.jpg", progress: 0,
-                youtube: "https://www.youtube.com/watch?v=cXg62-t8BWs"
+                img: "https://i.ytimg.com/vi/NMlJQsg-XLQ/maxresdefault.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=VyHV0BRtdxo"
+            },            
+            {
+                img: "https://static0.moviewebimages.com/wordpress/wp-content/uploads/2023/03/the-big-bang-theory.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=rCj-Fb1OmXg"
             },
             {
                 img: "https://i.ytimg.com/vi/OQgySPQ5M3Y/maxresdefault.jpg", progress: 0,
                 youtube: "https://www.youtube.com/watch?v=zckJCxYxn1g"
             },
-            {
-                img: "https://ingresso-a.akamaihd.net/img/cinema/cartaz/14413-destaque.jpg", progress: 0,
-                youtube: "https://www.youtube.com/watch?v=a06zxOyQrAs"
+             {
+                img: "https://anitrendz.net/news/wp-content/uploads/2025/03/princess-mononoke-imax-visual-scaled-e1741734957969.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=vf6c6n35wr4"
+            },
+             {
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgPkI8VBqYXkmX0qC7YlaQmb_TYZpICZSybA&s", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=HaLISMAGdOE"
             },
         ]
     },
     {
         title: "Séries",
         items: [
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+             { img: "https://s29288.pcdn.co/wp-content/uploads/2020/01/the-witcher-season-1-poster-750x298-1.jpg", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=ndl1W4ltcmg" },
+            { img: "https://m.economictimes.com/thumb/msid-99810432,width-1600,height-900,resizemode-4,imgsize-75530/black-mirror-season-6-on-netflix-release-date-and-all-you-may-want-to-know.jpg", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=V0XOApF5nLU" },
+            { img: "https://pop.proddigital.com.br/wp-content/uploads/sites/8/2023/04/01-75.jpg", top10: true, youtube: "https://www.youtube.com/watch?v=mXY4SH37RXc" },
+            { img: "https://www.ndgames.com.br/wp-content/uploads/2024/06/EN-US_Arcane_Payoff_Horizontal_YT_Banner_2560x1440_RGB_W1.0-1024x576.jpg", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=fXmAurh012s" },           
+            { img: "https://images8.alphacoders.com/487/487434.jpg", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=8QnMmpfKWvo" },
+            { img: "https://m.media-amazon.com/images/I/81f9Gi7rX7L.jpg", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=3gLlS9m4kns" },
         ]
     },
     {
         title: "Para maratonar",
         items: [
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAapBvFxmybHS8rArdvopwxK64zbiEgMpQwQ&s", top10: true, youtube: "https://www.youtube.com/watch?v=01WEqntM1NI" },
+            { img: "https://i.ytimg.com/vi/C0pWKVJobAE/maxresdefault.jpg", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wUFwunMKa4E" },
+            { img: "https://i0.wp.com/thefutureoftheforce.com/wp-content/uploads/2022/08/The-Sandman-Review-Netflix-Header.jpg?fit=1024%2C579&ssl=1", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=ZJXamnPUm0I" },
+            { img: "https://res.cloudinary.com/jerrick/image/upload/v1608420132/5fde8b2422d117001cd65f60.jpg", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=xl8zdCY-abw" },
+            { img: "https://thecomeback.com/wp-content/uploads/2016/12/Marco-Polo.jpg", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=TjxmgKuL7ZM" },
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0pceNRR0vQtWNWteMgzcNef4bnhfBNfLAHw&s", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=0DAmWHxeoKw" },
         ]
     }
-];
+],
+    Inês: [
+    {
+        title: "Mais assistidos",
+        items: [
+            {
+                img: "https://i.ytimg.com/vi/wmi9I4yiNiU/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDryETbcyb0yj45IxCxiYjushtxzw", top10: true, badge: "Clássico", badgeColor: "green", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=AJfhrLoQl5w"
+            },
+            {
+                img: "https://i.ytimg.com/vi/NMlJQsg-XLQ/maxresdefault.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=VyHV0BRtdxo"
+            },            
+            {
+                img: "https://static0.moviewebimages.com/wordpress/wp-content/uploads/2023/03/the-big-bang-theory.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=rCj-Fb1OmXg"
+            },
+            {
+                img: "https://i.ytimg.com/vi/OQgySPQ5M3Y/maxresdefault.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=zckJCxYxn1g"
+            },
+             {
+                img: "https://lightbox-prod.imgix.net/images/assets/BANNER_WIDE_mac_10292883_3E8551F5-EBF0-4154-AE37B5C75695C1BF.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=tD76OqlJRwQ"
+            },
+             {
+                img: "https://streamcoimg-a.akamaihd.net/000/390/618/390618-Banner-L2-794ccd06a39a5603e735d6ff6aac3ec8.jpeg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=2ZMGk_Ml1fc"
+            },
+        ]
+    },
+    {
+        title: "Séries",
+        items: [
+            { img: "https://pbs.twimg.com/media/G9kcXvJbcAA0C1v.jpg", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=IqaXNwAzSmQ" },
+            { img: "https://pop.proddigital.com.br/wp-content/uploads/sites/8/2023/04/01-75.jpg", top10: true, youtube: "https://www.youtube.com/watch?v=mXY4SH37RXc" },
+            { img: "https://images.thedirect.com/media/article_full/netf.jpg", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=DMNjH5MlQXc" },
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRz9qaoSG2BOo8ffY_wf26EWWWPeCOTH6BY-Q&s", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=Xn2PsXD8m4A" },
+            { img: "https://images.squarespace-cdn.com/content/v1/5ff3733e78965c3d27ca2956/07a18d56-e1ef-4e0c-84b7-8168d80876ed/queen-charlotte-new-wide.jpeg", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=oLtnNw0KT78" },
+            { img: "https://img-cdn.publive.online/fit-in/1200x675/afaqs/media/post_attachments/9360760fdc64a54dec46bfbdcafa925e4e3b007a00e9daa1ddc09fe9760c29f8.jpg", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=lptctjAT-Mk" },
+        ]
+    },
+    {
+        title: "Para maratonar",
+        items: [
+            { img: "https://knightwriters.org/wp-content/uploads/2022/03/gilmore-girls.jpg", top10: true, youtube: "https://www.youtube.com/watch?v=YwY86TlfDI0" },
+            { img: "https://m.media-amazon.com/images/M/MV5BZDhkNjMxN2EtNjIyZS00MmMzLTg3NzYtOWEzYTI3ZmQwOTI1XkEyXkFqcGc@._V1_QL75_UX388_.jpg", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=KfySe96FPr8" },
+            { img: "https://www.mundiario.com/media/cineseries/images/2025/09/08/2025090815194037389.jpg", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=AGF16szMOmo" },
+            { img: "https://media-cache.cinematerial.com/p/500x/fs4yqq8n/sweet-magnolias-movie-poster.jpg?v=1589418409", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=TEgrH93KLgY" },
+            { img: "https://upload.wikimedia.org/wikipedia/pt/6/62/The_Good_Doctor_2017.png", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=lnY9FWUTY84" },
+            { img: "https://cdn.europosters.eu/image/1300/art-photo/the-hangover-i161590.jpg", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=tcdUhdOlz9M" },
+        ]
+    }
+],
+    Maria: [
+    {
+        title: "Mais assistidos",
+        items: [
+            {
+                img: "https://occ-0-8574-5574.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABT5w_gEPSQsSJoiRPszryI6p6Qd-G7BsOECox1mf9uHejbMOfva6MgBG7QxKvleW5V9aIEk8iL-4P3975zrFNEAwG06d5DVOqVguyWN1wcNtAC6SqfJo9I_FndbcZ_ymI5CyJBiOIVvFpBL3gLIKi6pGC8EMYlQhY3EjaFinsbAxL5083EhoWHb6gX3OUUs1CJYzjZMSrCIjOA.jpg?r=944", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=AzCAwdp1uIQ"
+            },
+            {
+                img: "https://pastposters.com/cdn/shop/files/lilo-and-stitch-original-quad-movie-poster-jr-mm-2025-teaser-multi_7c874619-a6b0-40bb-b9af-be65f3189404.jpg?v=1753977612", top10: true, badge: "Clássico", badgeColor: "green", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=VWqJifMMgZE"
+            },
+            {
+                img: "https://wallpapers.com/images/hd/elsa-and-anna-movie-poster-54jqm799ygx1y4o1.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=Zi4LMpSDccc"
+            },            
+            {
+                img: "https://i0.wp.com/www.heyuguys.com/images/2010/10/rapunzel_18.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=ycoY201RTRo"
+            },            
+             {
+                img: "https://disney.images.edge.bamgrid.com/ripcut-delivery/v2/variant/disney/97b64170-56e2-4a1d-b958-fb17d9d1005f/compose?aspectRatio=1.78&format=webp&width=1200", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=tLtQ3bCvBA0"
+            },
+             {
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRf5QTJ-W1cM3YJpK5YATZ5dRoj-muq6KJfgg&s", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=pqdHP2dWQ9M"
+            },
+        ]
+    },
+    {
+        title: "Séries",
+        items: [
+            { img: "https://m.media-amazon.com/images/S/pv-target-images/0c105592c05d7ac63d1d29db571599002ec652e1747f87a79ddc4d1a4fa29d5f.png", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wsfex1UfRto" },
+            { img: "https://player.bbc.com/static/1614050245-106f5238ef39d4415ac2336252acbdc2691e707b.jpg", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=2ZtG5Ba6iT0" },
+            { img: "https://disney.images.edge.bamgrid.com/ripcut-delivery/v2/variant/disney/ee3b4cb8-50be-4fd8-be81-f9baef178c83/compose?aspectRatio=1.78&format=webp&width=1200", top10: true, youtube: "https://www.youtube.com/watch?v=EJ5ZvO63tg0" },            
+            { img: "https://disney.images.edge.bamgrid.com/ripcut-delivery/v2/variant/disney/34cc591f-ae1b-44ed-bf8a-c4bbef84d588/compose?format=webp&width=400", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=lFzVJEksoDY" },
+            { img: "https://occ-0-8574-5574.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABXfXKJwhVDiXL6_sM1rkYzUBiHajAICzOijmwHvgAB38V7Y29b1SV15swN9PDMMRo6oQQbgE4FYZlbFSpkiIfWwKFi_RyTNRc4A.jpg?r=b69", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=2eUeQI3BjNo" },
+            { img: "https://occ-0-8574-5574.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABe4KbpVTUHe8FKz9WWNk16Le2UQ4sX1q7yXKYNHVA9WDNIq7pfuUH9VyVN5RkAVnQNNNc7cmOVYbdN4nXDyJxG9yeziZ2MT6C8o.jpg?r=a9d", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=uX5DBxsmal0" },
+        ]
+    },
+    {
+        title: "Para maratonar",
+        items: [
+            { img: "https://i0.wp.com/rafu.com/wp-content/uploads/2017/01/sailor-moon-r.jpg", top10: true, youtube: "https://www.youtube.com/watch?v=XDnKDYpvEgo" },
+            { img: "https://disney.images.edge.bamgrid.com/ripcut-delivery/v2/variant/disney/3e8fdc10-1663-46b7-9dd2-0c368abcd46f/compose?aspectRatio=1.78&format=webp&width=1200", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=zG_8Djlplp8" },
+            { img: "https://i0.wp.com/animeprofile.com.br/wp-content/uploads/2025/01/image-21.png?resize=800%2C450&ssl=1", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=nS8-sAm9M9U" },
+            { img: "https://i.ytimg.com/vi/NMlJQsg-XLQ/maxresdefault.jpg", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=VyHV0BRtdxo" },
+            { img: "https://occ-0-8574-5574.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABQDMyu3AwHESJVJM02ApS-_FIZut26jXzBQ1Y8Q7WgOijGKrZLxLiyTjRqd92lOyl0gWS8BTLYLcTOSobNwMGPgICalB9wf6iug.jpg?r=b2e", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=EzKASl2T9d0" },
+            { img: "https://occ-0-8574-5574.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABULnQIjmfmkKdvuhcxYYwjHNmE2t6BI9lD4eJRdo7w_lOas3sGfyqEK76AecjaZvokdNrADQsxULAYtVTPUGHgtft-S9Hv-wG4I.webp?r=395", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=i-80SGWfEjM" },
+        ]
+    }
+],
+    Convidado: [
+        {
+            title: "Em destaque",
+            items: [ /* filmes do Convidado */ ]
+        }
+    ]
+};
